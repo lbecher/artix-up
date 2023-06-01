a5e8fadcbfd7dc2e1c13f2f16e944c260d8743d8
Luiz Fernando Becher de Araujo <luiz.becher.araujo@gmail.com>
2023-06-01 11:54:26 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+gname="artix"
+
+pefi="/dev/nvme0n1p1"
+pboot="/dev/nvme0n1p2"
+pluks="/dev/nvme0n1p3"
+
+loadkeys br-abnt2
+
+sv up ntpd
+
+cryptsetup -s 256 -h sha256 -c aes-xts-plain64 luksFormat $pluks
+cryptsetup luksOpen $pluks "${gname}_luks"
+
+pvcreate $plvm
+vgcreate $gname $plvm
+
+lvcreate -C y -L 8GB -n swap $gname
+lvcreate -C y -L 64GB -n root $gname
+lvcreate -C n -l 100%FREE -n home $gname
+
+mkswap /dev/$gname/swap
+
+mkfs.fat -F 32 $pefi
+fatlabel $pefi ESP
+
+mkfs.xfs $pboot
+mkfs.xfs /dev/$gname/root
+mkfs.xfs /dev/$gname/home
+
+mount /dev/$gname/root /mnt
+
+mkdir /mnt/efi
+mkdir /mnt/boot
+mkdir /mnt/home
+
+mount $pefi /mnt/efi
+mount $pboot /mnt/boot
+mount /dev/$gname/home /mnt/home
+
+swapon /dev/$gname/swap
+
+basestrap /mnt linux linux-firmware \
+    base base-devel runit elogind-runit \
+    grub os-prober efibootmgr nano \
+    NetworkManager
+
+fstabgen -U /mnt > /mnt/etc/fstab
